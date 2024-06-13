559f645322c97e2547d9f1fe8e5ef4e5533a9001
rrvv123 <eorms7391@gmail.com>
2024-06-13 14:40:38 +0900

    0613 - 예제 10-2 하기 전 이미지 먼저 업로드 후 커밋 먼저 하기 위한 작업

---
+++ app/src/main/java/kr/ac/kopo/ex10_2_0613/MainActivity.kt
@@ -0,0 +1,14 @@
+package kr.ac.kopo.ex10_2_0613
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
